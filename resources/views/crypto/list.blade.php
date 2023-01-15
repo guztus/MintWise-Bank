1bf7eb9f15a776ba55c1df4bf5b446289c7b1576
guztus <gustavs.zubars@gmail.com>
2023-01-16 00:07:12 +0200

    updates

--- resources/views/crypto/list.blade.php
+++ resources/views/crypto/list.blade.php
@@ -33,7 +33,8 @@
             class="table-rounded center w-full text-sm text-left text-gray-500 dark:text-gray-400 my-6 shadow-md">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th class="px-4 py-2">Logo</th>
+                <th class="px-4 py-2">#</th>
+                <th class="px-2 py-2">Logo</th>
                 <th class="px-4 py-2">Symbol</th>
                 <th class="px-4 py-2">Price</th>
                 <th class="px-4 py-2">1h %</th>
@@ -46,7 +47,10 @@
             <tbody>
             @foreach ($cryptoList as $cryptocurrency)
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <td class="px-4 py-3"><img src="{{ $cryptocurrency->getLogo() }}" alt="icon"></td>
+                    <td class="px-4 py-3">{{ $loop->index + 1 }}</td>
+                    <td class="px-2 py-3"><img src="{{ $cryptocurrency->getLogo() }}" alt="icon"
+                                               style="display: block; margin-left: auto; margin-right: auto; width: 50%;">
+                    </td>
                     <td class="px-4 py-3">
                         <a href="/crypto/{{ $cryptocurrency->getSymbol() }}">{{ $cryptocurrency->getSymbol() }}</a>
                     </td>
