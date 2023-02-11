2dbd834eaf0ac58880831c6f9155a2a7e56a9792
guztus <gustavs.zubars@gmail.com>
2023-02-12 01:13:20 +0200

    minor design fix

--- resources/views/wallet/partials/deposit-withdraw-form.blade.php
+++ resources/views/wallet/partials/deposit-withdraw-form.blade.php
@@ -71,13 +71,13 @@
 <div class="mt-4 flex flex-row justify-between w-max center" style="gap: 1.5em">
     <button type="submit"
             form="deposit"
-            class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">
+            class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800 mr-0">
         Deposit
     </button>
     <button type="submit"
             onclick="copyBuyForm()"
             form="withdraw"
-            class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
+            class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900 mr-0">
         Withdraw
     </button>
 </div>
